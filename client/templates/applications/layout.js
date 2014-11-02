f51426922e3b79319091ceb51580204159b5f097
Achmad Nazmy <nazmy@netxcel.com.sg>
2014-11-02 21:46:40 +0800

    Basic session

--- client/templates/applications/layout.js
+++ client/templates/applications/layout.js
@@ -1,3 +1,4 @@
 Template.layout.helpers({
 	pageTitle : function() {return Session.get('pageTitle');}
 });
+//Trigger in the browser console to set the session -> Session.set('pageTitle','Title Value');
